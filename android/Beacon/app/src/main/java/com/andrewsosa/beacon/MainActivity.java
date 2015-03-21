f3dd176df8c5be82e2f1d1835fd6a22c4089ef62
Andrew Sosa <sosa@cs.fsu.edu>
2015-03-21 09:54:41 -0400

    Updated Main activity and layout

--- android/Beacon/app/src/main/java/com/andrewsosa/beacon/MainActivity.java
+++ android/Beacon/app/src/main/java/com/andrewsosa/beacon/MainActivity.java
@@ -2,14 +2,18 @@ package com.andrewsosa.beacon;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity implements Toolbar.OnMenuItemClickListener{
@@ -17,7 +21,7 @@ public class MainActivity extends Activity implements Toolbar.OnMenuItemClickLis
     // Actionbar and Navdrawer nonsense
     ActionBarDrawerToggle mDrawerToggle;
     String[] fragments;
-    ListView nav_listView;
+    ListView mDrawerList;
     DrawerLayout drawerLayout;
 
     // Fragments
@@ -33,12 +37,10 @@ public class MainActivity extends Activity implements Toolbar.OnMenuItemClickLis
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.inflateMenu(R.menu.menu_main);
 
-
         // Drawer craziness
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
         drawerLayout.setDrawerListener(mDrawerToggle);
-
 
         // Add initial fragment
         if (findViewById(R.id.content_frame) != null) {
@@ -46,34 +48,38 @@ public class MainActivity extends Activity implements Toolbar.OnMenuItemClickLis
             // However, if we're being restored from a previous state,
             // then we don't need to do anything and should return or else
             // we could end up with overlapping fragments.
-            if (savedInstanceState != null) {
-                return;
+            if (savedInstanceState == null) {
+                // Create a new Fragment to be placed in the activity layout
+                mapViewFragment = new MapViewFragment();
+
+                // In case this activity was started with special instructions from an
+                // Intent, pass the Intent's extras to the fragment as arguments
+                //firstFragment.setArguments(getIntent().getExtras());
+
+                // Add the fragment to the 'fragment_container' FrameLayout
+                getFragmentManager().beginTransaction()
+                        .add(R.id.content_frame, mapViewFragment).commit();
             }
-
-            // Create a new Fragment to be placed in the activity layout
-            mapViewFragment = new MapViewFragment();
-
-            // In case this activity was started with special instructions from an
-            // Intent, pass the Intent's extras to the fragment as arguments
-            //firstFragment.setArguments(getIntent().getExtras());
-
-            // Add the fragment to the 'fragment_container' FrameLayout
-            getFragmentManager().beginTransaction()
-                    .add(R.id.content_frame, mapViewFragment).commit();
         }
 
 
-        fragments = getResources().getStringArray(R.array.fragments);
-        nav_listView = (ListView) findViewById(R.id.nav_listView);
 
-        // Set the adapter for the list view
-        nav_listView.setAdapter(new ArrayAdapter<String>(this,
+
+
+
+
+        /*
+        fragments = new String[] {"Map", "List"};
+        mDrawerList = (ListView) findViewById(R.id.nav_listView);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 R.layout.drawer_list_tile, R.id.tile_text, fragments));
-
-
-        // Set the list's click listener
-
-
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Helo", Toast.LENGTH_SHORT).show();
+                selectItem(position);
+            }
+        }); */
 
     }
 
@@ -89,6 +95,31 @@ public class MainActivity extends Activity implements Toolbar.OnMenuItemClickLis
     @Override
     public boolean onMenuItemClick(MenuItem menuItem) {
         return super.onOptionsItemSelected(menuItem);
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+        Fragment fragment = null;
+
+        if (position == 0) {
+            fragment = new MapViewFragment();
+        } else if (position == 1) {
+            fragment = new ListViewFragment();
+        }
+
+        Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+
+        // Insert the fragment by replacing any existing fragment
+        if (fragment != null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+        }
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        drawerLayout.closeDrawer(mDrawerList);
     }
 
 
