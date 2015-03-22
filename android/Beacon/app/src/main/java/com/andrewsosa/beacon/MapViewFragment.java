3768fb1961d8be29aeae0f41eb630da3db8d4792
Peternk92 <Peter@wg-dhcp149d163.fsuwin.wireless.fsu.edu>
2015-03-22 00:20:19 -0400

    Added startup current location stuff
    Auto aligns map to current location. EDITED ONLY MapViewFragment.java

--- android/Beacon/app/src/main/java/com/andrewsosa/beacon/MapViewFragment.java
+++ android/Beacon/app/src/main/java/com/andrewsosa/beacon/MapViewFragment.java
@@ -8,9 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.location.LocationManager;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.CameraUpdateFactory;
+
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -48,8 +55,41 @@ public class MapViewFragment extends BeaconFragment implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Log.d("Beacon", "onMapReady()");
- 
-        //googleMap.setMyLocationEnabled(true);
+
+        googleMap.setMyLocationEnabled(true);
+
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        // Create a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Get the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        // Get Current Location
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+
+        //set map type
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        // Get latitude of the current location
+        double latitude = myLocation.getLatitude();
+
+        // Get longitude of the current location
+        double longitude = myLocation.getLongitude();
+
+        // Create a LatLng object for the current location
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        // Show the current location in Google Map
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        // Zoom in the Google Map
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(20));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));
+
+
+
 
     }
 
